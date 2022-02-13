63303f780d576164982d45ad5354e7da4696532b
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-02-13 18:03:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+Given a string as input, print Consecutive:Yes, if two characters appear together lie L in HELLO, M in SUMMER, 0 in 9810098100 and Consecutive:No otherwise.
+Input-> "HELLO"
+Output-> Consecutive:Yes
+"""
